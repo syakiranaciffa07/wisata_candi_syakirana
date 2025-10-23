9a6b82bdd879bee2b1f5cb031689636f1627796e
syakiranaciffa07 <syakiranaciffakhairunissa_2327240114@mhs.mdp.ac.id>
2025-10-23 16:39:29 +0700

    detail screen

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:wisata_candi/data/candi_data.dart';
+import 'package:wisata_candi/screens/detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Wisata Candi',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: DetailScreen(candi: candiList[0]),
+    );
+  }
+}
+
+
